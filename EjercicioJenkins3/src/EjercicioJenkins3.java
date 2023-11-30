fc8aea25ae5a0da98f9395dd590f9c0b2b1fd23f
Pedro Ramos <1999pedrors@gmail.com>
2023-11-30 09:28:25 +0100

    EjercicioJenkins3

--- EjercicioJenkins3/src/EjercicioJenkins3.java
+++ EjercicioJenkins3/src/EjercicioJenkins3.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
 
 public class EjercicioJenkins3 {
 	public static void main(String[] args) {
-		System.out.println("A");
-	}
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Introduce tu nombre: ");
+        String nombre = sc.nextLine();
+        System.out.print("Introduce tu apellido: ");
+        String apellido = sc.nextLine();
+        System.out.print("Introduce tu edad: ");
+        int edad = sc.nextInt();
+
+        sc.close();
+        System.out.println("Nombre: " + nombre + ", Apellido: " + apellido + ", Edad: "+ edad);
+    }
 }
